d43fa3e2964d24a5282616544dcc06b8da0fedf0
forfed <maxime.fedou@gmail.com>
2021-02-16 13:36:34 +0100

    v1.0.1

---
+++ js/utils.js
@@ -0,0 +1,59 @@
+
+
+  export function getPromise(url) {
+
+  return new Promise(function (resolve,reject) {
+    
+    var req = new XMLHttpRequest()
+  req.open('GET', url, true)
+  req.onreadystatechange = function () {
+    if (req.readyState == 4) {
+       if(req.status == 200) {
+        resolve(JSON.parse(req.responseText))
+
+       }
+        
+       else
+         {
+            reject('error')
+         }
+    }
+  };
+  req.send(null)
+    
+ })
+
+    
+}
+
+
+  export function getProduct(element) {
+
+
+  let product = document.createElement("section")
+        product.classList.add("produit")
+        let product__image = document.createElement('img')
+        product__image.classList.add("product__image")
+        product__image.setAttribute("src",element.imageUrl)
+
+        let product__name = document.createElement('h3')
+        product__name.classList.add("product__name")
+        product__name.innerText = element.name;
+        let product__description = document.createElement('p')
+        product__description.classList.add("product__description")
+        product__description.innerText = element.description
+
+        let product__prix  = document.createElement('div')
+        product__prix.classList.add("product__prix")
+        product__prix.innerText = element.price
+        product.append(product__image,product__name,product__description,product__prix)
+        return product
+}
+
+
+
+
+
+
+
+
--- js/vue_liste.js
+++ js/vue_liste.js
@@ -1,44 +1,44 @@
-import {getPromise} from "./GetDatas.js"
-import {Menu} from "./Menu.js"
-Menu()
+import {getPromise,getProduct} from "./utils.js"
+
 
 getPromise('http://localhost:3000/api/cameras').then((res) => {
 
-displayCameras(res)
+displayCameras(res) 
+
+
+
 
 })
 
 const listeVue = document.getElementById('liste');
+const content = document.createElement("div")
+function displayCameras(products){
+    products.forEach(element => {
 
-function displayCameras(appareil){
-    for(let i in appareil) {
-        let product__card = document.createElement('section');
-            product__card.classList.add('product');
-            product__card.onclick = function storeData(){
-                window.localStorage.setItem('product__details', JSON.stringify(appareil[i]));
-                window.open("page_produit.html");
-            };
+       var product =  getProduct(element)
+        product.onclick = function storeData(){
+            window.localStorage.setItem('element', JSON.stringify(element))
+            window.open("page_produit.html");
+        };
+       
+        content.append(product)
 
-        let product__left__div = document.createElement('div');
-        let product__name = document.createElement('h3');
-            product__name.innerText = `${appareil[i].name}`;
+    
 
-        let product__image = document.createElement('img');
-            product__image.classList.add("product__image")
-            product__image.src = `${appareil[i].imageUrl}`;
-            console.log(product__image.src);
 
-        let product__right__div = document.createElement('div');
-        let product__price = document.createElement('p');
-            product__price.classList.add('product_price');
-            product__price.innerText = `${appareil[i].price}` + `€`;
 
-        let product__description = document.createElement('p');
-            product__description.innerText = `${appareil[i].description}`;
 
-        listeVue.append(product__card);
-        product__card.append(product__left__div,product__right__div);
-        product__left__div.append(product__name,product__image);
-        product__right__div.append(product__price,product__description);
-    }
+        
+    })
+
+    listeVue.append(content)
+    
+
 }
+
+
+
+  
+
+    
+    
